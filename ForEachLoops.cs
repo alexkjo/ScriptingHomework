d93e7fcf11e767baff7090d9e17142ec24f092ae
alexkjo <10681564@my.uvu.edu>
2017-03-01 21:22:38 -0700

    Added ForEachLoops

---
+++ ForEachLoops.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class ForEachLoops : MonoBehaviour {
+
+	void Start () {
+
+    int[] myInt = new int[3];
+
+    int numberOfPinkPlayers = 1;
+    int numberOfPurplePlayers = 3;
+    int numberOfTealPlayers = 2;
+
+    foreach(int item in int)
+        {
+            print(item);
+        }
+
+		
+	}
+
+}
