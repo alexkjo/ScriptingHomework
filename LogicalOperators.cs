25d485da27e1ff857d3dfa7c27e6117adfdbf428
alexkjo <10681564@my.uvu.edu>
2017-03-03 23:43:25 -0700

    Uploaded LogicalOperators Homework

---
+++ LogicalOperators.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOperators : MonoBehaviour {
+
+    public string BluePlayer;
+    public string RedPlayer;
+    public string LockedDoor;
+    public string Key;
+
+	// Use this for initialization
+	public void Start () {
+		
+        if (string == BluePlayer && string == RedPlayer)
+        {
+            print("Spawned both Players");
+        }
+
+        if (string == BluePlayer || string == RedPlayer)
+        {
+            print("One Player Spawned");
+        }
+
+        if (string != BluePlayer || string != RedPlayer)
+        {
+            print("Something went wrong");
+        }
+
+        if (string != BluePlayer && string != RedPlayer)
+        {
+            print("No Players spawned");
+        }
+
+        if (string == LockedDoor && string == Key)
+        {
+            print("Door is unlocked.");
+        }
+
+        if (string == LockedDoor || string == Key)
+        {
+            print("Door didn't Budge");
+        }
+
+        if (string != LockedDoor && string != Key)
+        {
+            print("What are you trying to do?");
+        }
+
+	}
+}
